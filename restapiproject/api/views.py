ddb3d3143e9d72dbef3fadd48e66f446ca6171d3
Melika Sadegh <meli.sadegh@gmail.com>
2024-08-06 21:06:14 +0330

    version 2

--- restapiproject/api/views.py
+++ restapiproject/api/views.py
@@ -1,7 +1,13 @@
 from rest_framework import generics
 from .models import PhoneNumber
 from .serializers import PhoneNumberSerializer
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, this is a sample API by Melika:)")
 
 class PhoneNumberListCreate(generics.ListCreateAPIView):
     queryset = PhoneNumber.objects.all()
     serializer_class = PhoneNumberSerializer
+
+
